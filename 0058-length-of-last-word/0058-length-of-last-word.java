0a4c7f28883e1aee2ed207c3ae7ef295f444615f
Utkarsh Bajpai <160762548+utkarshbajpaii@users.noreply.github.com>
2024-10-12 13:17:18 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (49.33%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -0,0 +1,8 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        s=s.trim();
+        String last=s.substring(s.lastIndexOf(' ')+1);
+        int n=last.length();
+        return n;
+    }
+}
